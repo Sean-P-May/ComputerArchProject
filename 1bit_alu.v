5152dcb17cbd7a2ff0572c3953e95de527b66879
SeanMay <sean.may.developer@gmail.com>
2026-09-26 09:39:26 -0400

    updated basic and finshed bitalu

--- 1bit_alu.v
+++ 1bit_alu.v
@@ -1,43 +1,80 @@
-module 1bitFullAdder(a,b,carry_in, sum, carry_out);
+/*
+ * Module: bitFullAdder
+ * Author: Sean May
+ * Description:
+ *   Implements a 1-bit full adder using basic logic gates.
+ *   Produces a sum and carry-out from inputs A, B, and carry-in.
+ */
+module bitFullAdder(a, b, carry_in, sum, carry_out);
   input a, b, carry_in;
   output sum, carry_out;
 
-  xor (a_xor_b, a,b);
-  and (a_and_b, a,b);
-  xor (sum, a_xor_b, a,b,);
+  wire a_xor_b;
+  wire a_and_b;
+  wire carry_and;
+
+  xor (a_xor_b, a, b);
+  and (a_and_b, a, b);
+  xor (sum, a_xor_b, carry_in);
   and (carry_and, a_xor_b, carry_in);
   or (carry_out, a_and_b, carry_and);
 
 endmodule
 
 
-
-module 1bitALU(a,b,carry_in, less, op, out, carry_out);
+/*
+ * Module: bitALU
+ * Author: Sean May
+ * Description:
+ *   Implements a 1-bit ALU with support for AND, OR, addition,
+ *   and set-less-than operations. Inputs A and B can also be
+ *   inverted using the ALU control signals.
+ */
+module bitALU(a, b, carry_in, less, op, out, carry_out);
   input a, b, carry_in, less;
-  input op[0:3]; // invert a, invert b, op0, op2
+  input [0:3] op; // invert A, invert B, operation select
   output out, carry_out;
 
-  wire logic_out[0:3]
-  not(not_a,a);
-  not(n0t_b,b);
+  wire [0:3] logic_out;
+  wire not_a;
+  wire not_b;
+  wire a_nota;
+  wire b_notb;
 
-  muilplexer2x1 negplex_a([a, nota], op[0], a_nota);
-  muilplexer2x1 negplex_b([b,notbi], op[1], b_notb);
-  
-  and(logic_out[0], a_nota, b_notb);
-  or(logic_out[1], a_nota, b_notb);
+  // Generate inverted versions of A and B
+  not (not_a, a);
+  not (not_b, b);
 
-  1bitFullAdder full_adder(a_nota, b_notb, carry_in, logic_out[2], carry_out);
+  // Select normal or inverted inputs
+  multiplexer2x1 negplex_a(a, not_a, op[0], a_nota);
+  multiplexer2x1 negplex_b(b, not_b, op[1], b_notb);
 
-  buf(logic_out[3]);
-  
-  multiplexer4X1 (logic_out,op[2:3],output);
+  // AND operation
+  and (logic_out[0], a_nota, b_notb);
+
+  // OR operation
+  or (logic_out[1], a_nota, b_notb);
+
+  // ADD operation
+  bitFullAdder full_adder(
+    a_nota,
+    b_notb,
+    carry_in,
+    logic_out[2],
+    carry_out
+  );
+
+  // Set-less-than result
+  buf (logic_out[3], less);
+
+  // Select final ALU result
+  multiplexer4X1 result_mux(
+    logic_out,
+    op[2:3],
+    out
+  );
 
 endmodule
-
-
-
-
 
 
 
--- basic_components.v
+++ basic_components.v
@@ -1,42 +1,81 @@
-// 2-to-1 multiplexer built from primitive logic gates.
-// S = 0 selects i0, and S = 1 selects i1.
-// Author: Sean May
-module multiplexer2x1 (i0, i1, S, Out);
 
-  input i0, i1, S;
+/*
+ * Module: multiplexer2x1
+ * Author: Sean May
+ * Description:
+ *   Implements a 2-to-1 multiplexer using primitive logic gates.
+ *   S = 0 selects i[0], and S = 1 selects i[1].
+ */
+module multiplexer2x1 (
+    i,
+    S,
+    Out
+);
+
+  input [1:0] i;
+  input S;
   output Out;
 
   wire notS, i0_and_S, i1_and_S;
 
-  // Generate both selection paths before combining them.
   not (notS, S);
-  and (i0_and_S, i0, notS);
-  and (i1_and_S, i1, S);
+  and (i0_and_S, i[0], notS);
+  and (i1_and_S, i[1], S);
   or (Out, i0_and_S, i1_and_S);
 
 endmodule
 
 
-// 4-to-1 multiplexer constructed from three 2-to-1 multiplexers.
-// s0 selects within each pair, while s1 selects between the two pairs.
-// Author: Sean May
-module multiplexer4x1 (i0, i1, i2, i3, s0, s1, Out);
+/*
+ * Module: multiplexer4x1
+ * Author: Sean May
+ * Description:
+ *   Implements a 4-to-1 multiplexer using three 2-to-1 multiplexers.
+ *   s[0] selects within each input pair, while s[1] selects
+ *   between the two pairs.
+ */
+module multiplexer4x1 (
+    i,
+    s,
+    Out
+);
 
-  input i0, i1, i2, i3, s0, s1;
+  input [3:0] i;
+  input [1:0] s;
   output Out;
 
   wire out1, out2;
 
-  multiplexer2x1 mux0 (i0, i1, s0, out1);
-  multiplexer2x1 mux1 (i2, i3, s0, out2);
-  multiplexer2x1 mux2 (out1, out2, s1, Out);
+  // Select between i[0]/i[1] and i[2]/i[3]
+  multiplexer2x1 mux0 (
+      i[1:0],
+      s[0],
+      out1
+  );
+  multiplexer2x1 mux1 (
+      i[3:2],
+      s[0],
+      out2
+  );
+
+  // Select between the results of the first two multiplexers
+  multiplexer2x1 mux2 (
+      {out2, out1},
+      s[1],
+      Out
+  );
 
 endmodule
 
 
+
 // Level-sensitive D latch constructed from NAND gates.
 // When C is high, Q follows D. When C is low, the stored value is held.
-module D_latch (D, C, Q);
+module D_latch (
+    D,
+    C,
+    Q
+);
 
   input D, C;
   output Q;
@@ -51,7 +90,11 @@ endmodule
 
 
 // Negative-edge-triggered D flip-flop built from two D latches.
-module D_flip_flop (D, CLK, Q);
+module D_flip_flop (
+    D,
+    CLK,
+    Q
+);
 
   input D, CLK;
   output Q;
@@ -60,8 +103,15 @@ module D_flip_flop (D, CLK, Q);
 
   not not1 (CLK1, CLK);
 
-  D_latch D1 (D, CLK, Y);
-  D_latch D2 (Y, CLK1, Q);
+  D_latch D1 (
+      D,
+      CLK,
+      Y
+  );
+  D_latch D2 (
+      Y,
+      CLK1,
+      Q
+  );
 
 endmodule
-
